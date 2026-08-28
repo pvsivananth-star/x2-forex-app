9dbc5a347273abbbc4f2435f5a4923fcc5697bd2
pvsivananth-star <pvsivananth@gmail.com>
2026-08-28 22:16:06 +0530

    export crypto catalog service

--- mobile/src/services/rates/index.ts
+++ mobile/src/services/rates/index.ts
@@ -1,5 +1,6 @@
 export * from './types';
 export * from './fxService';
 export * from './cryptoService';
+export * from './cryptoCatalogService';
 export * from './metalsService';
 export * from './equityService';
