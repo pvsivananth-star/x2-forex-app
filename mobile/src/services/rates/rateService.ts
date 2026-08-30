32dc6f9377eea3bbf2d4af8bcdee71cd701b9d58
pvsivananth-star <pvsivananth@gmail.com>
2026-08-30 20:51:16 +0530

    restore rate service temporarily

--- mobile/src/services/rates/rateService.ts
+++ mobile/src/services/rates/rateService.ts
@@ -1,1 +1,7 @@
-// RESTORE PENDING
+import type {IRateService} from './IRateService';
+
+export class RateService implements IRateService {
+    async refreshRates(category: any, tenor: any): Promise<any> {
+        throw new Error('Temporary restore placeholder');
+    }
+}
