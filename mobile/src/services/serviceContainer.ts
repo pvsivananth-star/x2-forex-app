b27f7e6fdb6d3e00ab43d9025b780f4d839fead9
pvsivananth-star <pvsivananth@gmail.com>
2026-08-30 20:21:39 +0530

    cleanup: remove unused persistence model import

--- mobile/src/services/serviceContainer.ts
+++ mobile/src/services/serviceContainer.ts
@@ -5,7 +5,6 @@ import {RateService} from './rates/RateService';
 import {PersistenceService} from './persistence/PersistenceService';
 import type {StoredMobileState} from './persistence/mobileStateStorage';
 import {CatalogService} from './catalog/CatalogService';
-import type {PersistedSettings} from '../models';
 
 export interface ApplicationServices {
     rates: IRateService;
